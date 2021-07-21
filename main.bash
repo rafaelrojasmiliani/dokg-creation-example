a708a08b0d90b7cc302143806ee61befe5655759
fraunhoferlab <rafael.rojas@fraunhofer.it>
2021-07-21 11:52:59 +0200

    wip

--- main.bash
+++ main.bash
@@ -2,10 +2,13 @@
 
 
 deps_that_rosdep_cant_find(){
+    # Find all the packages that
+    # a package depedns on and that
+    # rosdep cannot find
     local __array=$2
     local __result=""
     declare -a unresolved_packages
-    for p in $(rosdep keys $1); do 
+    for p in $(pack_deps $1); do 
         if ! $(rosdep resolve $p  >/dev/null 2>&1); then
             __result="$__result $p"
         fi
@@ -13,7 +16,43 @@ deps_that_rosdep_cant_find(){
     eval $__array="'$__result'"
 }
 
+pack_deps(){
+    rosdep keys $1
+}
+
+get_pack_deps_not_installed_with_apt(){
+
+    local __result=""
+
+    for p in $(rosdep keys $1); do
+        if $(rospack find $p >/dev/null 2>&1); then
+            addr=$(rospack find $p 2>/dev/null)
+            if [[ $addr != "/opt/ros"* ]]; then
+                __result="$__result $p"
+            fi
+#        else
+#            __result="$__result $p"
+        fi
+    done
+
+    echo $__result
+
+
+}
+
+get_pack_to_produce_deb(){
+
+    local __result=$(get_pack_deps_not_installed_with_apt)
+
+}
+
+
+
 main(){
+
+    tem_rosdep_yaml="temp_rosdep.yaml"
+    temp_rosdep_list="/etc/ros/rosdep/sources.list.d/99-temp.list"
+
 
     scriptdir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
     if [ "$scriptdir" != "$(pwd)" ]; then
@@ -21,39 +60,47 @@ main(){
         exit 1
     fi
 
-    #catkin build || exit 1
+    catkin build || exit 1
 
 
-    #source devel/setup.bash
+    source devel/setup.bash
 
-#    if [ -f "./rosdep.yaml" ]; then
-#        sudo bash -c "echo \"yaml file://$(realpath rosdep.yaml)\" > /etc/ros/rosdep/sources.list.d/10-my-local.list"
-#        rosdep update
-#    fi
+    if [ -f "./rosdep.yaml" ]; then
+        sudo bash -c "echo \"yaml file://$(realpath rosdep.yaml)\" > /etc/ros/rosdep/sources.list.d/10-my-local.list"
+        rosdep update
+    fi
 #
 #
     mapfile -t desired_packages < ./desired_packages.txt
     req_pack=""
     for package in ${desired_packages[@]}; do
-        deps_that_rosdep_cant_find $package result
-        req_pack="$req_pack $result"
+        req_pack="$req_pack $package"
+        req_pack="$req_pack $(get_pack_deps_not_installed_with_apt $package)"
     done
     req_pack=$(echo "$req_pack" | xargs -n1 | sort -u | xargs)
-    
-    for pin in ${req_pack}; do
-        echo $pin
+
+    rm $tem_rosdep_yaml 2>/dev/null
+    echo --- > $tem_rosdep_yaml 
+    for p in $req_pack; do
+        envsubst <<EOF >> $tem_rosdep_yaml
+$p:
+    ubuntu:
+        apt: [$(echo $p | sed 's/_/-/g' | sed 's/\(.*\)/ros-'$(lsb_release -cs)'-\1/' )]
+EOF
     done
-#
-#    echo ${desired_packages[@]}
-#
-#
-#
-#
-#    for ros_package in ${desired_packages[@]}; do
-#        roscd $ros_package
-#        #bloom-generate rosdebian --os-name ubuntu --os-version $(lsb_release -cs) --ros-distro $ROS_DISTRO
-#        echo ------ $(echo $(pwd)) -----
-#    done
+
+    sudo bash -c "echo \"yaml file://$(realpath $tem_rosdep_yaml)\" > $temp_rosdep_list"
+    rosdep update
+    
+
+    
+
+
+    for p in $req_pack; do
+        roscd $p
+        bloom-generate rosdebian --os-name ubuntu --os-version $(lsb_release -cs) --ros-distro $ROS_DISTRO
+        fakeroot debian/rules "binary --parallel"
+    done
 
     return 0;
 }
